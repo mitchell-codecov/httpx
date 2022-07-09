59a0ad84534166c33c140489b6b33c355d8d8cc1
Mitchell Borrego <mitchell@codecov.io>
2022-07-09 19:58:15 -0400

    refactor: remove unused variable

--- httpx/_urls.py
+++ httpx/_urls.py
@@ -533,7 +533,6 @@ class QueryParams(typing.Mapping[str, str]):
 
         value = args[0] if args else kwargs
 
-        items: typing.Sequence[typing.Tuple[str, PrimitiveData]]
         if value is None or isinstance(value, (str, bytes)):
             value = value.decode("ascii") if isinstance(value, bytes) else value
             self._dict = parse_qs(value)
